c18079e6a7fcd51eb0be19dcd149403519e923a4
David Ivy <Davidivy003@gmail.com>
2019-08-27 09:44:38 -0400

    Add csv parsing for input example

--- find_open_restaurants.py
+++ find_open_restaurants.py
@@ -1,4 +1,16 @@
+import csv
+
 def find_open_restuarants():
     print('Hello World')
+
+def parse_csv():
+    input_data = []
+
+    with open('examples/input1.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            input_data.append(row)
+
+    return input_data
 
 find_open_restuarants()
